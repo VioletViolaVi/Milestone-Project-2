c75b6f55cdbde659c7111203034526ddb2a6a664
VioletViolaVi <vivian.v18723@gmail.com>
2020-06-19 13:55:57 +0000

    Added function to make cards flip back when they don't match.

--- js/scripts.js
+++ js/scripts.js
@@ -7,32 +7,43 @@ $("document").ready(function () {
 
   // flips cards when clicked
   $(".wholeCard").click(function () {
-    $(this).toggleClass("flip"); 
+    $(this).toggleClass("flip");
   });
 
- // prints out name of clicked animal card
- $(".wholeCard").click(function (){
-   console.log($(this).data("image"));
-    
+  // prints out name of clicked animal card
+  $(".wholeCard").click(function () {
+    console.log($(this).data("image"));
   });
 
- // matching cards
- $(".wholeCard").click(function (){ 
-     $(this).addClass("clicked");
-      // picking two cards
-     if ($(".clicked").length === 2) {
-         // checking data of the two cards
-         if ($(".clicked").first().data("image")===$(".clicked").last().data("image") ) {
-             // removes matched cards and hides them
-                 $(".clicked").addClass("hide").removeClass("clicked");        
-             console.log("these 2 cards ARE the same");         
-         }else{
-             $(".clicked").removeClass("clicked"); 
-        console.log("these 2 cards are NOT the same");
-         }
-     }
+  // do cards match or not?
+  $(".wholeCard").click(function () {
+    $(this).addClass("clicked");
+    // picking two cards
+    if ($(".clicked").length === 2) {
+      // checking data of the two cards
+      if (
+        $(".clicked").first().data("image") ===
+        $(".clicked").last().data("image")
+      ) {
+        // removes matched cards by hiding them
+        $(".clicked").addClass("hide").removeClass("clicked");;
+      } else {
+        // flips back non matching cards
+        toFlipBackUnmatchedCards();
+        // removes clicked class so "length === 2" continues to work
+        $(".clicked").removeClass("clicked");
+      }
+    }
   });
 
+  /********************** called out functions in "do cards match or not?" */
+
+  // function & flips back non matching cards
+  function toFlipBackUnmatchedCards() {
+    setTimeout(function () {
+      $(".flip").toggleClass("flip");
+    }, 1000);
+  }
 
 
 
@@ -52,29 +63,42 @@ $("document").ready(function () {
 
 
 
+  //  // removes clicking ability after two picked out cards
+  //  function removeClickingOnCards() {
+  //      $(".wholeCard").click(function(){
+  //          $(this).off("click");
+  //      });          removeClickingOnCards();
+  //  };
+
+  //  // brings back clicking ability after two picked out cards dissapper or flip back
+  //  function restoreClickingOnCards() {
+  //      $(".wholeCard").click(function(){
+  //          $(this).on("click");
+  //      });             restoreClickingOnCards();
+  //  };
 
 
 
 
-//   //node.list of cards
-//   let animals = document.querySelectorAll(".wholeCard");
-//   // converts node.list to array
-//   let animalArray = Array.from(animals);
-//   // Fisher-Yates shuffle method
-//   function shufflingCards() {
-//     // declaring the card position picked at random
-//     let randomlyPicked;
-//     // declaring the card position picked started from the back going backwards
-//     let decrementallyPicked;
-//     // last item in the array; swaps with items in position numbers greater than zero; going backwards in the array
-//     for (i = animalArray.length - 1; i > 0; i--) {
-//       let randomlyPicked = Math.floor(Math.random() * 12);
-//       let decrementingCard = animalArray[i];
-//       animalArray[i] = animalArray[randomlyPicked];
-//       animalArray[randomlyPicked] = decrementingCard;
-//     }  
-//   }
 
 
 
+  //   //node.list of cards
+  //   let animals = document.querySelectorAll(".wholeCard");
+  //   // converts node.list to array
+  //   let animalArray = Array.from(animals);
+  //   // Fisher-Yates shuffle method
+  //   function shufflingCards() {
+  //     // declaring the card position picked at random
+  //     let randomlyPicked;
+  //     // declaring the card position picked started from the back going backwards
+  //     let decrementallyPicked;
+  //     // last item in the array; swaps with items in position numbers greater than zero; going backwards in the array
+  //     for (i = animalArray.length - 1; i > 0; i--) {
+  //       let randomlyPicked = Math.floor(Math.random() * 12);
+  //       let decrementingCard = animalArray[i];
+  //       animalArray[i] = animalArray[randomlyPicked];
+  //       animalArray[randomlyPicked] = decrementingCard;
+  //     }
+  //   }
 });
